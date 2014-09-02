ff8e31487ec2e2507c7a066875647df7690bc00c
Иван Мигунов <d3a4t8e@yandex.com>
2014-09-02 15:56:08 +0400

    Комманды создания и удаления бд
    # manage.py createdb
    # manage.py dropdb

--- manage.py
+++ manage.py
@@ -1,9 +1,19 @@
-from deanthe import app, manager
+from deanthe import app, manager, db
 
 @manager.command
 def runserver():
 	""" Start the development server """
 	app.run(host = '0.0.0.0', port = 5000)
 
+@manager.command
+def createdb():
+	""" Create database """
+	db.create_all()
+
+@manager.command
+def dropdb():
+	""" Drop database """
+	db.drop_all()
+	
 if __name__ == '__main__':
 	manager.run()
