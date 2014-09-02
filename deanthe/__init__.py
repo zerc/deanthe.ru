5baf0caea10f9295800767835474a327bd9a8208
Иван Мигунов <d3a4t8e@yandex.com>
2014-09-02 15:49:54 +0400

    Пакет для общения с базой данны�
    flask-sqlalchemy

--- deanthe/__init__.py
+++ deanthe/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from os.path import join, split
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.root_path = '/'.join(app.root_path.split('/')[:-1])
@@ -8,3 +9,4 @@ app.config.from_object('deanthe.config.DevelopmentConfig')
 
 url = app.add_url_rule
 manager = Manager(app)
+db = SQLAlchemy(app)
