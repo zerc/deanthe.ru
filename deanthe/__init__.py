d0ce0aeb5809c55c46ae61f15ef31382c468354c
Иван Мигунов <d3a4t8e@yandex.com>
2014-09-02 15:33:16 +0400

    Модуль управления url адресами
    deanthe.ru\urls.py

--- deanthe/__init__.py
+++ deanthe/__init__.py
@@ -4,3 +4,5 @@ from os.path import join, split
 app = Flask(__name__)
 app.root_path = '/'.join(app.root_path.split('/')[:-1])
 app.config.from_object('deanthe.config.DevelopmentConfig')
+
+url = app.add_url_rule
