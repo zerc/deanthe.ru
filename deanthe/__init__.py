02b52c3cfa1698a576a7621c971b32aea7947c05
Иван Мигунов <d3a4t8e@yandex.com>
2014-09-02 15:48:19 +0400

    Пакет для менеджера управления проектом
    flask-script

--- deanthe/__init__.py
+++ deanthe/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from os.path import join, split
+from flask.ext.script import Manager
 
 app = Flask(__name__)
 app.root_path = '/'.join(app.root_path.split('/')[:-1])
 app.config.from_object('deanthe.config.DevelopmentConfig')
 
 url = app.add_url_rule
+manager = Manager(app)
--- manage.py
+++ manage.py
@@ -1,4 +1,9 @@
-from deanthe import app
+from deanthe import app, manager
+
+@manager.command
+def runserver():
+	""" Start the development server """
+	app.run(host = '0.0.0.0', port = 5000)
 
 if __name__ == '__main__':
-	app.run()
+	manager.run()
